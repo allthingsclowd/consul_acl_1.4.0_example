3c006f063a0084ad04deb4e687553ebad9af51fc
Graham Land <graham@allthingscloud.eu>
2020-01-06 13:40:13 +0000

    Updated for multi-cert generation

--- scripts/generate_certificates.sh
+++ scripts/generate_certificates.sh
@@ -58,19 +58,19 @@ install_cfssl () {
 create_default_templates () {
     # Reset the directory contents - let's hope you've saved your keys!!!
     DATE=`date +"%T"`
-    [ -d /usr/local/bootstrap/certificate-config ] && mv /usr/local/bootstrap/certificate-config /tmp/certificate-config${DATE}
-    mkdir -p /usr/local/bootstrap/certificate-config
+    [ -d /usr/local/bootstrap/certificate-config/${1} ] && mv /usr/local/bootstrap/certificate-config/${1} /tmp/certificate-config/${1}${DATE}
+    mkdir -p /usr/local/bootstrap/certificate-config/${1}
     
-    cd /usr/local/bootstrap/certificate-config
+    cd /usr/local/bootstrap/certificate-config/${1}
     # Generate a default Certificate Signing Request (CSR)
     #cfssl print-defaults config > ca-config.json
-    create_new_ca-config
-    cfssl print-defaults csr > ca-csr.json
+    create_new_ca-config ${1}
+    cfssl print-defaults csr > ${1}-ca-csr.json
 }
 
 create_new_ca-config () {
     
-    tee /usr/local/bootstrap/certificate-config/ca-config.json <<EOF
+    tee /usr/local/bootstrap/certificate-config/${1}/${1}-ca-config.json <<EOF
 {
     "signing": {
         "default": {
@@ -110,81 +110,63 @@ EOF
 }
 
 create_required_certificates () {
-
-    create_default_templates
+    # ${1} - product (e.g. "consul")
+    # ${2} - hostname (e.g. "\"leader01\"" )
+    # ${3} - hosts (e.g. "\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.6.11\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\"" )
     
-    pushd /usr/local/bootstrap/certificate-config
+    create_default_templates ${1}
+    
+    pushd /usr/local/bootstrap/certificate-config/${1}
 
 
     # Step 1 - Create a Certificate Authority
     #########################################
 
     # Customise the CSR
-    # Set algo to RSA and key size 2048
-    update_key_in_json_file ca-csr.json ".key.algo" "\"rsa\""
-    update_key_in_json_file ca-csr.json ".key.size" 2048
-    update_key_in_json_file ca-csr.json ".CN" "\"hashistack.ie\""
-    #update_key_in_json_file ca-csr.json ".hosts" "[\"allthingscloud.eu\",\"github.com/allthingsclowd\"]"
+    # Set algo to RSA and key size 4096
+    update_key_in_json_file ${1}-ca-csr.json ".key.algo" "\"rsa\""
+    update_key_in_json_file ${1}-ca-csr.json ".key.size" 4096
+    update_key_in_json_file ${1}-ca-csr.json ".CN" "\"hashistack.ie\""
     update_key_in_json_file ca-csr.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
 
     # Generate the Certificate Authorities's (CA's) private key and certificate
-    cfssl gencert -initca ca-csr.json | cfssljson -bare hashistack-ca -
+    cfssl gencert -initca ${1}-ca-csr.json | cfssljson -bare ${1}-hashistack-ca -
 
     # This should generate hashistack_ca-key, hashistack_ca.csr, hashistack_ca.pem
 
 #     # Step 2 - Generate Server Certificate
-    cfssl print-defaults csr > server.json
+    cfssl print-defaults csr > ${1}-server.json
     
-    update_key_in_json_file server.json ".key.algo" "\"rsa\""
-    update_key_in_json_file server.json ".key.size" 2048
-    update_key_in_json_file server.json ".CN" "\"leader01\""
-    update_key_in_json_file server.json ".hosts" "[\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\"]"
-    update_key_in_json_file server.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
+    update_key_in_json_file ${1}-server.json ".key.algo" "\"rsa\""
+    update_key_in_json_file ${1}-server.json ".key.size" 4096
+    update_key_in_json_file ${1}-server.json ".CN" "${2}"
+    update_key_in_json_file ${1}-server.json ".hosts" "[${3}]"
+    update_key_in_json_file ${1}-server.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
 
-    cfssl gencert -ca=hashistack-ca.pem -ca-key=hashistack-ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare hashistack-server
+    cfssl gencert -ca=${1}-hashistack-ca.pem -ca-key=${1}-hashistack-ca-key.pem -config=${1}-ca-config.json -profile=server ${1}-server.json | cfssljson -bare ${1}-hashistack-server
 
 #     # Step 3 - Generate Peer Certificate
-    cfssl print-defaults csr > peer.json
+    cfssl print-defaults csr > ${1}-peer.json
     
-    update_key_in_json_file peer.json ".key.algo" "\"rsa\""
-    update_key_in_json_file peer.json ".key.size" 2048
-    update_key_in_json_file peer.json ".CN" "\"leader01\""
-    update_key_in_json_file peer.json ".hosts" "[\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\"]"
-    update_key_in_json_file peer.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
+    update_key_in_json_file ${1}-peer.json ".key.algo" "\"rsa\""
+    update_key_in_json_file ${1}-peer.json ".key.size" 4096
+    update_key_in_json_file ${1}-peer.json ".CN" "\"server\"" # changed from hostname
+    update_key_in_json_file ${1}-peer.json ".hosts" "[${3}]"
+    update_key_in_json_file ${1}-peer.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
 
-    cfssl gencert -ca=hashistack-ca.pem -ca-key=hashistack-ca-key.pem -config=ca-config.json -profile=peer peer.json | cfssljson -bare hashistack-peer
+    cfssl gencert -ca=${1}-hashistack-ca.pem -ca-key=${1}-hashistack-ca-key.pem -config=${1}-ca-config.json -profile=peer ${1}-peer.json | cfssljson -bare ${1}-hashistack-peer
 
 #     # Step 4 - Generate Client Certificate
-    cfssl print-defaults csr > client.json
+    cfssl print-defaults csr > ${1}-client.json
     
-    update_key_in_json_file client.json ".key.algo" "\"rsa\""
-    update_key_in_json_file client.json ".key.size" 2048
-    update_key_in_json_file client.json ".CN" "\"client\""
-    update_key_in_json_file client.json ".hosts" "[\"\"]"
-    update_key_in_json_file client.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
+    update_key_in_json_file ${1}-client.json ".key.algo" "\"rsa\""
+    update_key_in_json_file ${1}-client.json ".key.size" 4096
+    update_key_in_json_file ${1}-client.json ".CN" "\"client\""
+    update_key_in_json_file ${1}-client.json ".hosts" "[\"\"]"
+    update_key_in_json_file ${1}-client.json ".names" "[{\"C\" : \"UK\",\"ST\" : \"Shropshire\",\"L\" : \"Pontesbury\"}]"
 
-    cfssl gencert -ca=hashistack-ca.pem -ca-key=hashistack-ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare hashistack-client
-
-
-
-
-#     # admin policy hcl definition file
-#     tee cfssl.json <<EOF
-#     {
-#     "signing": {
-#         "default": {
-#             "expiry": "87600h",
-#             "usages": [
-#                 "signing",
-#                 "key encipherment",
-#                 "server auth",
-#                 "client auth"
-#                 ]
-#             }
-#         }
-#     }
-# EOF
-    
+    cfssl gencert -ca=${1}-hashistack-ca.pem -ca-key=${1}-hashistack-ca-key.pem -config=${1}-ca-config.json -profile=client ${1}-client.json | cfssljson -bare ${1}-hashistack-client
+   
 #     # Generate a certificate for the Consul server
 #     echo '{"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=hashistack-ca.pem -ca-key=hashistack-ca-key.pem -config=cfssl.json \
 #     -hostname="hashistack.ie,192.168.9.11,192.168.*.*,81.143.215.2,localhost,127.0.0.1" - | cfssljson -bare hashistack-server
@@ -197,10 +179,10 @@ create_required_certificates () {
 #     echo '{"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=hashistack-ca.pem -ca-key=hashistack-ca-key.pem -profile=client \
 #     - | cfssljson -bare hashistack-cli
 
-#     # wrap certs as p12 for chrome browser
-#     openssl pkcs12 -password pass:bananas -export -out hashistack-server.pfx -inkey hashistack-server-key.pem -in hashistack-server.pem -certfile hashistack-ca.pem
-#     openssl pkcs12 -password pass:bananas -export -out hashistack-client.pfx -inkey hashistack-client-key.pem -in hashistack-client.pem -certfile hashistack-ca.pem
-#     openssl pkcs12 -password pass:bananas -export -out hashistack-cli.pfx -inkey hashistack-cli-key.pem -in hashistack-cli.pem -certfile hashistack-ca.pem
+    # wrap certs as p12 for chrome browser
+    openssl pkcs12 -password pass:bananas -export -out ${1}-hashistack-server.pfx -inkey ${1}-hashistack-server-key.pem -in ${1}-hashistack-server.pem -certfile ${1}-hashistack-ca.pem
+    openssl pkcs12 -password pass:bananas -export -out ${1}-hashistack-client.pfx -inkey ${1}-hashistack-client-key.pem -in ${1}-hashistack-client.pem -certfile ${1}-hashistack-ca.pem
+    openssl pkcs12 -password pass:bananas -export -out ${1}-hashistack-peer.pfx -inkey ${1}-hashistack-peer-key.pem -in ${1}-hashistack-peer.pem -certfile ${1}-hashistack-ca.pem
     
     pwd
     ls -al
@@ -210,4 +192,6 @@ create_required_certificates () {
 
 install_golang
 install_cfssl
-create_required_certificates
+create_required_certificates "consul" "\"leader01\"" "\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.6.11\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\"" 
+create_required_certificates "vault" "\"leader01\"" "\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.6.11\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\""
+create_required_certificates "nomad" "\"leader01\"" "\"leader01.allthingscloud.eu\",\"127.0.0.1\",\"192.168.6.11\",\"192.168.9.11\",\"81.143.215.2\",\"leader01\",\"hashistack.ie\"" 
